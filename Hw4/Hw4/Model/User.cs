a157fe9ccda8ba2c28b1db1abe8391a91a7ba55f
tayebmoradi <tayeb.mora69@gmail.com>
2023-05-20 21:53:38 +0330

    create folder modle and user class

--- Hw4/Hw4/Model/User.cs
+++ Hw4/Hw4/Model/User.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace Hw4.Model
 {
-    internal class User
+    public class User
     {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Dateofbirth { get; set; }
+        public DateTime CreateUser { get; set; }
+        public User(int id, string fullName, string phoneNumber, string dateofbirth, DateTime createUser)
+        {
+            this.Id = id;
+            this.FullName = fullName;
+            this.PhoneNumber = phoneNumber;
+            this.Dateofbirth = dateofbirth;
+            this.CreateUser = createUser;    
+        }
     }
 }
--- Hw4/Hw4/Program.cs
+++ Hw4/Hw4/Program.cs
@@ -10,6 +10,7 @@ namespace Hw4
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
